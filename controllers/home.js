d5a73f03807cbaac278d82a669ebaf5bbe86a3ac
Dave Laird <kiwiupover@gmail.com>
2018-12-12 09:59:27 -0600

    redirect passthrough

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,13 @@
-/**
- * GET /
- * Home page.
- */
+const path = require('path');
+
+const awsUrl = 'http://s3-us-west-2.amazonaws.com/outdoorsy-demo-assets/static';
+
 exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home'
-  });
+  // headers.get('ODC-PARTNER-ID');
+
+  const { file } = req.query;
+  const partnerId = req.headers['ODC-PARTNER-ID'];
+  const redirectUrl = path(awsUrl, partnerId, file);
+
+  res.redirect(redirectUrl);
 };
