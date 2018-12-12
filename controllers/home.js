01f54734b76be205b48ca0988b8c409bef114edd
Dave Laird <kiwiupover@gmail.com>
2018-12-12 10:43:38 -0600

    adding redirect

--- controllers/home.js
+++ controllers/home.js
@@ -5,9 +5,22 @@ const awsUrl = 'http://s3-us-west-2.amazonaws.com/outdoorsy-demo-assets/static';
 exports.index = (req, res) => {
   // headers.get('ODC-PARTNER-ID');
 
-  const { file } = req.query;
-  const partnerId = req.headers['ODC-PARTNER-ID'];
-  const redirectUrl = path(awsUrl, partnerId, file);
+  const file = req.query.file || 'footer.html';
+  const partnerId = req.headers['ODC-PARTNER-ID'] || '1234-1234-1234';
+  const redirectUrl = path.join(awsUrl, partnerId, file);
+
+  res.render('home', {
+    title: redirectUrl,
+    redirectUrl
+  });
+};
+
+exports.redirect = (req, res) => {
+  // headers.get('ODC-PARTNER-ID');
+
+  const file = req.query.file || 'footer.html';
+  const partnerId = req.headers['ODC-PARTNER-ID'] || '1234-1234-9999';
+  const redirectUrl = path.join(awsUrl, partnerId, file);
 
   res.redirect(redirectUrl);
 };
