8557fcd7973ae29771a5a0fcc670cd721da07f30
Dave Laird <kiwiupover@gmail.com>
2018-12-12 10:52:49 -0600

    we are almost there

--- controllers/home.js
+++ controllers/home.js
@@ -1,13 +1,16 @@
 const path = require('path');
 
-const awsUrl = 'http://s3-us-west-2.amazonaws.com/outdoorsy-demo-assets/static';
+const awsUrl = 's3-us-west-2.amazonaws.com/outdoorsy-demo-assets/static';
 
 exports.index = (req, res) => {
   // headers.get('ODC-PARTNER-ID');
 
   const file = req.query.file || 'footer.html';
-  const partnerId = req.headers['ODC-PARTNER-ID'] || '1234-1234-1234';
-  const redirectUrl = path.join(awsUrl, partnerId, file);
+  const partnerId = req.headers['odc-partner-id'] || '1234-1234-1234';
+  const urlPath = path.join(awsUrl, partnerId, file);
+  const redirectUrl = `http://${urlPath}`;
+
+  console.log('req.headers', req.headers);
 
   res.render('home', {
     title: redirectUrl,
@@ -19,8 +22,9 @@ exports.redirect = (req, res) => {
   // headers.get('ODC-PARTNER-ID');
 
   const file = req.query.file || 'footer.html';
-  const partnerId = req.headers['ODC-PARTNER-ID'] || '1234-1234-9999';
-  const redirectUrl = path.join(awsUrl, partnerId, file);
+  const partnerId = req.headers['odc-partner-id'] || '1234-1234-9999';
+  const urlPath = path.join(awsUrl, partnerId, file);
+  const redirectUrl = `http://${urlPath}`;
 
   res.redirect(redirectUrl);
 };
